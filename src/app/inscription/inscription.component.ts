7d79c9710d224adc95b24e71d4fc0e6fd5c49c76
fbansept <bansept.franck@gmail.com>
2024-05-22 11:50:41 +0200

    gestion erreur

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,8 +1,16 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inscription',
@@ -26,6 +34,9 @@ export class InscriptionComponent {
     password: ['root', [Validators.required]],
   });
 
+  snackBar: MatSnackBar = inject(MatSnackBar);
+  router: Router = inject(Router);
+
   onInscription() {
     if (this.formulaire.valid) {
       this.http
@@ -33,7 +44,38 @@ export class InscriptionComponent {
           'http://localhost/backend_angular_devweb1_24/inscription.php',
           this.formulaire.value
         )
-        .subscribe((resultat: any) => console.log(resultat));
+        .subscribe({
+          next: (resultat: any) => {
+            this.snackBar.open(
+              'Vous êtes inscrit, veuillez vous connecter',
+              undefined,
+              {
+                panelClass: 'snack-bar-valid',
+                duration: 3000,
+              }
+            );
+
+            this.router.navigateByUrl('/connexion');
+          },
+          error: (erreur: any) => {
+            //si l'email est déjà existant
+            if (erreur.status == 409) {
+              this.snackBar.open('Cet email est déjà utilisé', undefined, {
+                panelClass: 'snack-bar-warning',
+                duration: 3000,
+              });
+            } else {
+              this.snackBar.open(
+                "Erreur inconnue, veuillez contacter l'administrateur",
+                undefined,
+                {
+                  panelClass: 'snack-bar-error',
+                  duration: 3000,
+                }
+              );
+            }
+          },
+        });
     }
   }
 }
